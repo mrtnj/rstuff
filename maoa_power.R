eec035f539911e3281e41059f78afd1ff2c9e658
mrtnj <pepparspray@yahoo.se>
2016-11-17 19:48:49 +0100

    Subgroup analysis

--- maoa_power.R
+++ maoa_power.R
@@ -4,6 +4,8 @@
 library(plyr)
 library(broom)
 library(magrittr)
+library(reshape2)
+library(ggplot2)
 
 data <- data.frame(genotype = c("low", "low", "low", "high", "high", "high"),
                    maltreatment = c("no", "probable", "severe", "no", "probable", "severe"),
@@ -28,10 +30,10 @@ expanded_data <- ddply(data, c("genotype", "maltreatment"), function(x) {
              conduct_disorder = c(rep(1, x$conduct_disorder),
                                   rep(0, x$N - x$conduct_disorder)))
 })
-expanded_data$exposure <- ifelse(expanded_data$maltreatment == "no", "no", "yes")
 
-ddply(subset(expanded_data, genotype == "low"), c("exposure", "genotype"), summarise,
-      conduct_disorder = sum(conduct_disorder), n = length(exposure))
+
+ddply(subset(expanded_data, maltreatment == "severe"), "genotype", summarise,
+      conduct_disorder = sum(conduct_disorder), n = length(genotype))
 
 model <- glm(conduct_disorder ~ genotype * maltreatment,
              expanded_data, family = binomial(link = "logit"))
@@ -48,19 +50,20 @@ colnames(estimates) <- c("OR", "lower", "upper", "coef")
 ## Rautiainen 2016 GWAS 1.6
 ## Ficks & Waldman 2013 1.2 Metaanalysis of marginal MAOA 
 
-simplified_model <- glm(conduct_disorder ~ exposure,
-                        subset(expanded_data, genotype == "low"), family = binomial(link = "logit"))
+simplified_model <- glm(conduct_disorder ~ genotype,
+                        subset(expanded_data, maltreatment == "severe"),
+                        family = binomial(link = "logit"))
 
 
 (simplified_estimates <- exp(cbind(coef(simplified_model), confint(simplified_model))))
 
-simulate_data <- function(OR, p_exposure = 55/108, N = 55+108) {
-  x <- rbinom(N, 1, p_exposure)
-  exposure <- ifelse(x == 1, "yes", "no")
+simulate_data <- function(OR, p_low = 13/(20+13), N = 20+13) {
+  x <- rbinom(N, 1, p_low)
+  genotype <- ifelse(x == 1, "low", "high")
   z <- log(0.33) + log(OR) * x
   p <- 1/(1 + exp(-z)) 
   y <- rbinom(N, 1, p) 
-  data.frame(exposure, conduct_disorder = y)
+  data.frame(genotype, conduct_disorder = y)
 }
 
 
@@ -70,21 +73,21 @@ fake_data <- function(OR, N = 1000) {
 
 fake_glms <- function(fake_data) {
   results <- ldply(fake_data, function(x) {
-    tidy(glm(conduct_disorder ~ exposure, x, family = binomial(link = "logit")))
+    tidy(glm(conduct_disorder ~ genotype, x, family = binomial(link = "logit")))
   })
-  subset(results, term == "exposureyes")
+  subset(results, term == "genotypelow")
 }
 
 power_summary <- function(fake_glms, true_OR) {
   significant <- subset(fake_glms, p.value < 0.05)
   data.frame(power = nrow(significant)/nrow(fake_glms),
-             exaggeration = exp(mean(significant$estimate))/true_OR,
+             mean_estimate = exp(mean(significant$estimate)),
              sign_error = nrow(subset(significant, estimate < 0))/nrow(fake_glms))
 }
 
 
 power_simulations <- ldply(seq(from = 1.1, to = 3, by = 0.1),
-  function(x) fake_data(x) %>% fake_glms %>% power_summary(true_OR = x))
+  function(x) fake_data(x) %>% fake_glms %>% power_summary())
 power_simulations$OR <- seq(from = 1.1, to = 3, by = 0.1)
 
 melted <- melt(power_simulations[, -2], id.vars = "OR")
@@ -92,5 +95,5 @@ melted <- melt(power_simulations[, -2], id.vars = "OR")
 power_plot <- qplot(x = OR, y = value, data = melted, colour = variable) +
   ylim(0, 1) + geom_smooth(se = FALSE)
 
-exaggeration_plot <- qplot(x = OR, y = exaggeration, data = power_simulations) +
+exaggeration_plot <- qplot(x = OR, y = mean_estimate, data = power_simulations) +
   geom_smooth(se = FALSE)
