dadc6cec9fc5890ff39089e19af8a921fdd3d7e6
mrtnj <pepparspray@yahoo.se>
2016-12-02 19:00:39 +0100

    Contrasts

--- maoa_power.R
+++ maoa_power.R
@@ -45,8 +45,7 @@ estimates$coef <- rownames(estimates)
 colnames(estimates) <- c("OR", "lower", "upper", "coef")
 
 
-## Caspi 2002 OR 2.8 (1.42 - 5.74)
-## Caspi maltreatment probable 1.3, severe 2.5
+## Caspi 2002 maltreatment probable 1.3, severe 2.5
 ## Rautiainen 2016 GWAS 1.6
 ## Ficks & Waldman 2013 1.2 Metaanalysis of marginal MAOA 
 
@@ -60,7 +59,7 @@ simplified_model <- glm(conduct_disorder ~ genotype,
 simulate_data <- function(OR, p_low = 13/(20+13), N = 20+13) {
   x <- rbinom(N, 1, p_low)
   genotype <- ifelse(x == 1, "low", "high")
-  z <- log(0.33) + log(OR) * x
+  z <- -0.2 + log(OR) * x
   p <- 1/(1 + exp(-z)) 
   y <- rbinom(N, 1, p) 
   data.frame(genotype, conduct_disorder = y)
@@ -82,7 +81,7 @@ power_summary <- function(fake_glms, true_OR) {
   significant <- subset(fake_glms, p.value < 0.05)
   data.frame(power = nrow(significant)/nrow(fake_glms),
              mean_estimate = exp(mean(significant$estimate)),
-             sign_error = nrow(subset(significant, estimate < 0))/nrow(fake_glms))
+             sign_error = nrow(subset(significant, exp(estimate) < 1))/nrow(fake_glms))
 }
 
 
@@ -90,10 +89,9 @@ power_simulations <- ldply(seq(from = 1.1, to = 3, by = 0.1),
   function(x) fake_data(x) %>% fake_glms %>% power_summary())
 power_simulations$OR <- seq(from = 1.1, to = 3, by = 0.1)
 
-melted <- melt(power_simulations[, -2], id.vars = "OR")
 
-power_plot <- qplot(x = OR, y = value, data = melted, colour = variable) +
-  ylim(0, 1) + geom_smooth(se = FALSE)
+power_plot <- qplot(x = OR, y = power, data = power_simulations) +
+  ylim(0, 1) + geom_smooth(se = FALSE) + theme_bw()
 
 exaggeration_plot <- qplot(x = OR, y = mean_estimate, data = power_simulations) +
-  geom_smooth(se = FALSE)
+  ylim(0, 10) + geom_smooth(se = FALSE) + theme_bw()
