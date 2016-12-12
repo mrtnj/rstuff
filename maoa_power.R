cb60b7f19b6e5daf16531a5e687f83e6c00ddf21
mrtnj <pepparspray@yahoo.se>
2016-12-12 19:17:00 +0100

    Coefficients

--- maoa_power.R
+++ maoa_power.R
@@ -56,18 +56,18 @@ simplified_model <- glm(conduct_disorder ~ genotype,
 
 (simplified_estimates <- exp(cbind(coef(simplified_model), confint(simplified_model))))
 
-simulate_data <- function(OR, p_low = 13/(20+13), N = 20+13) {
-  x <- rbinom(N, 1, p_low)
+simulate_data <- function(OR, intercept, p_low, N) {
+  x <- c(rep(0, round(N * (1 - p_low))), rep(1, round(N * p_low)))
   genotype <- ifelse(x == 1, "low", "high")
-  z <- -0.2 + log(OR) * x
-  p <- 1/(1 + exp(-z)) 
+  z <- intercept + log(OR) * x
+  p <- 1/(1 + exp(-z))
   y <- rbinom(N, 1, p) 
   data.frame(genotype, conduct_disorder = y)
 }
 
 
-fake_data <- function(OR, N = 1000) {
-  replicate(N, simulate_data(OR = OR), simplify = FALSE)
+fake_data <- function(OR, intercept = -0.2, nsim = 1000, p_low = 13/(20+13), N = 20+13) {
+  replicate(nsim, simulate_data(OR = OR, intercept = intercept, p_low, N), simplify = FALSE)
 }
 
 fake_glms <- function(fake_data) {
@@ -81,7 +81,7 @@ power_summary <- function(fake_glms, true_OR) {
   significant <- subset(fake_glms, p.value < 0.05)
   data.frame(power = nrow(significant)/nrow(fake_glms),
              mean_estimate = exp(mean(significant$estimate)),
-             sign_error = nrow(subset(significant, exp(estimate) < 1))/nrow(fake_glms))
+             sign_error = nrow(subset(significant, exp(estimate) < 1))/nrow(significant))
 }
 
 
@@ -90,8 +90,36 @@ power_simulations <- ldply(seq(from = 1.1, to = 3, by = 0.1),
 power_simulations$OR <- seq(from = 1.1, to = 3, by = 0.1)
 
 
-power_plot <- qplot(x = OR, y = power, data = power_simulations) +
-  ylim(0, 1) + geom_smooth(se = FALSE) + theme_bw()
+power_plots <- function(power_simulations) {
+  list(power_plot = qplot(x = OR, y = power, data = power_simulations) +
+         ylim(0, 1) + geom_smooth(se = FALSE) + theme_bw(),
+       sign_error_plot = qplot(x = OR, y = sign_error, data = power_simulations) +
+         ylim(0, 1) + geom_smooth(se = FALSE) + theme_bw(),
+       exaggeration_plot = qplot(x = OR, y = mean_estimate, data = power_simulations) +
+         geom_smooth(se = FALSE) + theme_bw())
+}
 
-exaggeration_plot <- qplot(x = OR, y = mean_estimate, data = power_simulations) +
-  ylim(0, 10) + geom_smooth(se = FALSE) + theme_bw()
+small_power_plots <- power_plots(power_simulations)
+
+large_power_simulations <- ldply(seq(from = 1.1, to = 3, by = 0.1),
+  function(x) fake_data(x, nsim = 1000, N = 500) %>%
+  fake_glms %>% power_summary())
+large_power_simulations$OR <- seq(from = 1.1, to = 3, by = 0.1)
+
+
+large_power_plots <- power_plots(large_power_simulations)
+
+
+## Meta-analysis
+library(metafor)
+
+fake_meta_data <- fake_data(2, nsim = 15)
+fake_meta_summarised <- ldply(fake_meta_data, function(x) {
+  data.frame(tpos = sum(x$genotype == "low" & x$conduct_disorder == 1),
+             tneg = sum(x$genotype == "low" & x$conduct_disorder == 0),
+             cpos = sum(x$genotype == "high" & x$conduct_disorder == 1),
+             cneg = sum(x$genotype == "high" & x$conduct_disorder == 0))
+})
+
+escalc(measure = "OR", ai = tpos, bi = tneg, ci = cpos,
+       di = cneg, data = fake_meta_summarised, append = FALSE) %>% rma
