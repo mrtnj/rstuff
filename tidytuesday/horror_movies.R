195b71630da990a9d0679b70b01d895b0983155c
Martin Johnsson <sorill@gmail.com>
2019-10-21 20:24:57 +0100

    Make plots of horror movies

---
+++ tidytuesday/horror_movies.R
@@ -0,0 +1,136 @@
+
+## Horror movie data 2019-10-22
+## https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-10-22
+
+library(dplyr)
+library(egg)
+library(ggplot2)
+library(lubridate)
+library(readr)
+library(stringr)
+
+
+
+movies <- read_csv("horror_movies.csv")
+
+
+## Parse dates
+
+movies$release_parsed  <- parse_date(movies$release_date,
+                                     format = "%d-%b-%y",
+                                     locale = locale("en")) 
+
+
+movies$release_year <- ifelse(is.na(movies$release_parsed),
+                              movies$release_date,
+                              year(movies$release_parsed))
+
+movies$release_month  <- month.abb[month(movies$release_parsed)]
+
+
+## Parse budget
+
+movies$budget_currency  <- str_match(movies$budget,
+                                     "^[^0-9]+")[,1]
+
+movies$budget_numeric  <- as.numeric(gsub(movies$budget, pattern = "[^0-9]",
+                                          replacement = ""))
+
+## Count films per year
+
+count <- as.data.frame(table(movies$release_month))
+
+colnames(count) <- c("release_month", "number")
+
+
+## Linear model of average rating
+
+model  <- lm(review_rating ~ release_month, movies)
+
+fit  <- data.frame(release_month = month.abb,
+                   predict(model,
+                           newdata = data.frame(release_month = month.abb),
+                           interval = "confidence"),
+                   stringsAsFactors = FALSE)
+
+grand_mean_rating  <- mean(movies$review_rating,
+                           na.rm = TRUE)
+
+## Linear model of log budget
+
+model_budget  <- lm(log10(budget_numeric) ~ release_month, movies)
+
+fit_budget  <- data.frame(release_month = month.abb,
+                          predict(model_budget,
+                                  newdata = data.frame(release_month = month.abb),
+                                  interval = "confidence"),
+                          stringsAsFactors = FALSE)
+
+
+
+
+plot_number <- ggplot() +
+    geom_bar(aes(x = release_month,
+                   y = number),
+             data = count,
+             stat = "identity",
+             fill = "grey") +
+    scale_x_discrete(limits = month.abb) +
+    theme_bw(base_size = 12) +
+    theme(panel.grid = element_blank()) +
+    xlab("") +
+    ylab("Number of films released")
+    
+
+plot_rating <- ggplot() +
+    geom_violin(aes(x = release_month,
+                    y = review_rating),
+                fill = "grey",
+                colour = NA,
+                data = movies) +
+    scale_x_discrete(limits = month.abb) +
+    geom_pointrange(aes(x = release_month,
+                        y = fit,
+                        ymax = upr,
+                        ymin = lwr),
+                    data = fit) +
+    geom_hline(yintercept = grand_mean_rating,
+               linetype = 2,
+               colour = "red") +
+    ylim(0, 10) +
+    theme_bw(base_size = 12) +
+    theme(panel.grid = element_blank()) +
+    xlab("") +
+    ylab("Review rating")
+
+
+plot_budget <- ggplot() +
+    geom_violin(aes(x = release_month,
+                    y = log10(budget_numeric)),
+                data = filter(movies,
+                              budget_currency == "$"),
+                fill = "grey",
+                colour = NA) +
+    geom_pointrange(aes(x = release_month,
+                        y = fit,
+                        ymax = upr,
+                        ymin = lwr),
+                    data = fit_budget) +
+    scale_x_discrete(limits = month.abb) +
+    scale_y_continuous(breaks = c(4, 6, 8),
+                       labels = c(0.01, 1, 100)) +
+    theme_bw(base_size = 12) +
+    theme(panel.grid = element_blank()) +
+    xlab("Release month") +
+    ylab("Million dollars budget")
+
+
+plot_combined <- ggarrange(plot_number,
+                           plot_rating,
+                           plot_budget,
+                           top = "September and October are the horror film months")
+
+
+pdf("movies.pdf")
+print(plot_combined)
+dev.off()
