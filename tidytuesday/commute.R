694a15053b502dad9c4a28a83011ec8ec1207def
Martin Johnsson <sorill@gmail.com>
2019-11-09 07:58:36 +0000

    Make a map of Sweden for comparison

--- tidytuesday/commute.R
+++ tidytuesday/commute.R
@@ -5,8 +5,9 @@
 
 library(dplyr)
 library(ggplot2)
-library(maps)
 library(readr)
+library(tidyr)
+library(swemaps)
 library(usmap)
 
 
@@ -61,4 +62,60 @@ plot_map  <- ggplot() +
 
 pdf("commute_map.pdf", width = 14)
 print(plot_map)
+dev.off()
+
+
+
+## Sweden
+
+commute_sweden <- read_csv("commute_sweden.csv",
+                           comment = "#")
+colnames(commute_sweden)[2:3]  <- c("walking", "bike") 
+
+## Duplicate Kalmar--Gotland row
+ix  <- which(commute_sweden$namn == "Kalmar län;Gotlands län")
+
+extra_rows  <- commute_sweden[c(ix, ix),]
+extra_rows$namn  <- unlist(strsplit(extra_rows$namn[1],
+                                    split =  ";"))
+
+commute_sweden_modified  <- rbind(commute_sweden[-ix,],
+                                  extra_rows)
+
+## Separate the two variables
+commute_sweden_long  <-  pivot_longer(commute_sweden_modified,
+                                      c("walking", "bike"),
+                                      names_to = "variable",
+                                      values_to = "value")
+
+combined_sweden <- full_join(commute_sweden_long,
+                             map_ln,
+                             by = c("namn" = "lnnamn"))
+
+
+plot_sweden <- ggplot() +
+    geom_polygon(aes(x = ggplot_long,
+                     y = ggplot_lat,
+                     group = namn,
+                     fill = value),
+                 colour = "black",
+                 data = combined_sweden) +
+    coord_equal() +
+    facet_wrap(~ variable) +
+    scale_fill_continuous(low = "white",
+                          high = "blue",
+                          name = "Percent commuters") +
+    theme_bw(base_size = 16) +
+    theme(panel.grid = element_blank(),
+          strip.background = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          legend.position = "bottom") +
+    xlab("") +
+    ylab("") +
+    labs(caption = "Swedish commuters by län 2005-2006. Data from VTI.")
+
+
+pdf("commute_sweden_map.pdf")
+print(plot_sweden)
 dev.off()
